7cf8a19093259fe5de3deb41fe2fb3d4a2cf4bf3
MegaInformatica2022 <109810178+MegaInformatica2022@users.noreply.github.com>
2022-07-26 14:49:56 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 '''
 
-Writer Classic
+Writer
 
 Made in Python 3
 
@@ -11,11 +11,9 @@ Developed by: Mega Informática
 
 #Importar os módulos necessários
 from tkinter import *
-from tkinter.ttk import *
 import tkinter.filedialog as dlg
 import tkinter.messagebox as mb
 import webbrowser
-import datetime
 
 #Criar a janela
 janela = Tk()
@@ -28,22 +26,8 @@ with open('data/'+str(thisLang)+'.txt', 'r', encoding='utf-8') as myLang2:
     myLang = myLang2.read()
     dd = myLang.split('//')
 
-with open('config/menu.txt', 'r', encoding='utf-8') as menuColor:
-    mBg = menuColor.read()
-
-with open('config/mfg.txt', 'r', encoding='utf-8') as menuFg:
-    mFg = menuFg.read()
-
-with open('config/menu.txt', 'r', encoding='utf-8') as corLeitura:
-    corMenu = corLeitura.read()
-
-with open('config/mfg.txt', 'r', encoding='utf-8') as corFgLer:
-    colorFg = corFgLer.read()
-
 #Configurar o texto e a janela
 janela.title(dd[0])
-
-
 
 texto = Text(janela)
 
@@ -74,14 +58,7 @@ ccc.close()
 langset.close()
 
 
-def janelageometrica1(indicar,usar,nao):
-    indicar.geometry(str(usar)+'x'+str(nao))
-    with open('config/geom.txt', 'w') as geomdata:
-        geomdata.write('')
-        geom = geomdata.write(str(usar)+'x'+str(nao))
-    geomdata.close()
-
-def mudacor(cor, fgcor, cc, bar, bar_sec):
+def mudacor(cor, fgcor, cc):
     with open('config/colour.txt', 'w') as again1:
         again1.write('')
         again1.write(cor)
@@ -93,42 +70,6 @@ def mudacor(cor, fgcor, cc, bar, bar_sec):
         again2.write(cc)
     texto.configure(bg=cor, fg=fgcor, insertbackground=cc)
     texto.pack()
-    with open('config/menu.txt', 'w') as again4:
-        again4.write('')
-        again4.write(bar)
-    with open('config/mfg.txt', 'w') as again5:
-        again5.write('')
-        again5.write(bar_sec)
-    again1.close()
-    again2.close()
-    again3.close()
-    again4.close()
-    again5.close()
-    aguardaResposta = mb.askyesno(parent=janela, title='Writer Classic - Exit now?', message='You must reopen Writer to apply the changes!\nRestart now?')
-    if aguardaResposta:
-        janela.destroy()
-
-def mudaIdioma(idioma, raiz):
-    with open('config/lang.txt', 'w') as deleteThat:
-        deleteThat.write('')
-        deleteThat.write(idioma)
-    kkkkSemRiso = mb.askyesno(parent=raiz, title='Writer Classic - Exit now?', message='You must reopen Writer to apply the changes!\nRestart now?')
-    if kkkkSemRiso:
-        raiz.destroy()
-
-def formata_tudo(raiz):
-    pois = mb.askyesno(title='Writer Classic - Delete data', message="Are you sure you want to delete Writer Classic's data?")
-    if pois:
-        ficheiro = dlg.askopenfilename(parent=raiz, filetypes = [(dd[1], '*.txt'), (dd[2], '*.html'), ('Python Script', '*.py'), ('JavaScript File', '*.js')])
-        fich_teste = open(ficheiro, 'r')
-        fich_test = fich_teste.read()
-        if len(fich_test) != 0:
-            fich_saida = open(ficheiro, 'w')
-            fich_saida.write('')
-            fich_saida.close()
-        else:
-            mb.showinfo(title=dd[36], message=dd[35])
-            fich_teste.close()
 
 def new_window():
     newWindow = Toplevel(janela)
@@ -139,18 +80,6 @@ def new_window():
     with open('data/'+str(thisLang)+'.txt', 'r', encoding='utf-8') as myLang2:
         myLang = myLang2.read()
         dd = myLang.split('//')
-
-    with open('config/menu.txt', 'r', encoding='utf-8') as menuColor:
-        mBg = menuColor.read()
-
-    with open('config/mfg.txt', 'r', encoding='utf-8') as menuFg:
-        mFg = menuFg.read()
-
-    with open('config/menu.txt', 'r', encoding='utf-8') as corLeitura:
-        corMenu = corLeitura.read()
-
-    with open('config/mfg.txt', 'r', encoding='utf-8') as corFgLer:
-        colorFg = corFgLer.read()
 
     #Configurar o texto e a newWindow
     newWindow.title(dd[38])
@@ -191,7 +120,7 @@ def website():
     webbrowser.open(ourMainWebsite, 2)
 
 def repo():
-    ourRepo = "https://github.com/MegaInformatica2022/WriterClassic/"
+    ourRepo = "https://github.com/MegaInformatica2022/Writer/"
 
     webbrowser.open(ourRepo, 2)
 
@@ -222,23 +151,38 @@ def relogio():
 
     relog.mainloop()
 
+def mudaIdioma(idioma, raiz):
+    with open('config/lang.txt', 'w') as deleteThat:
+        deleteThat.write('')
+        deleteThat.write(idioma)
+    kkkkSemRiso = mb.askyesno(parent=raiz, title='Writer 5.0.0 - Exit now?', message='You must reopen Writer to apply the changes!\nRestart now?')
+    if kkkkSemRiso:
+        raiz.destroy()
+
+def formata(raiz):
+    with open('config/lang.txt', 'w', encoding='utf-8') as a1:
+        a1.write('en')
+    with open('config/geom.txt', 'w', encoding='utf-8') as a2:
+        a2.write('600x400')
+    with open('config/fg.txt', 'w', encoding='utf-8') as a3:
+        a3.write('#a5a9e8')
+    with open('config/colour.txt', 'w', encoding='utf-8') as a4:
+        a4.write('#020421')
+    with open('config/ct.txt', 'w', encoding='utf-8') as a5:
+        a5.write('#a5a9e8')
+    kkkSemRiso = mb.askyesno(parent=raiz, title='Writer 5.0.0 - Exit now?', message='You must reopen Writer to apply the changes!\nRestart now?')
+    if kkkSemRiso:
+        raiz.destroy()
+
 def abrir(raiz):
     fich = dlg.askopenfilename(parent=raiz, filetypes=[('Text files', '*.txt'), ('HTML Files', '*.html'), ('Python Script', '*.py'), ('Java Source Code', '*.java'), ('Javascript File', '*.js')])
     fich_ent2 = open(fich, 'r')
     resultado = fich_ent2.read()
-    if len(resultado) > 1:
-        abertura = Toplevel(janela)
-        abertura.title(dd[3])
-        abertura.geometry('615x500')
-        quadrado = Frame(abertura)
-        quadrado.pack()
-        etiqueta_aberta = Label(quadrado, text=resultado, wrap=600)
-        etiqueta_aberta.pack()
-        #mb.showinfo(title=dd[3], message=resultado)
+    if len(resultado) > 0:
+        mb.showinfo(title=dd[3], message=str(dd[4])+'\n\n'+str(resultado))
         fich_entrada = open(fich,'a')
         fich_ent2.close()
     else:
-        mb.showinfo(title=dd[36], message=dd[35])
         fich_entrada = open(fich,'a')
         fich_ent2.close()
 
@@ -260,15 +204,9 @@ def formatar(raiz):
     pois = mb.askyesno(title=dd[5], message=dd[6])
     if pois:
         ficheiro = dlg.askopenfilename(parent=raiz, filetypes = [(dd[1], '*.txt'), (dd[2], '*.html'), ('Python Script', '*.py'), ('JavaScript File', '*.js')])
-        fich_teste = open(ficheiro, 'r')
-        fich_test = fich_teste.read()
-        if len(fich_test) != 0:
-            fich_saida = open(ficheiro, 'w')
-            fich_saida.write('')
-            fich_saida.close()
-        else:
-            mb.showinfo(title=dd[36], message=dd[35])
-            fich_teste.close()
+        fich_saida = open(ficheiro, 'w')
+        fich_saida.write('')
+        fich_saida.close()
 
 def sair(raiz):
     confirm = mb.askyesno(title=dd[7], message=dd[8])
@@ -289,11 +227,11 @@ def sobre(thing2, thing3):
     mb.showinfo(title=dd[10], message=about_data)
     about_d.close()
 
-'''def sobreisto(thing4):
-    with open(thing4, 'r', encoding='utf-8') as about_d:
-        about_data = about_d.read()
-    mb.showinfo(title=dd[10], message=about_data)
-    about_d.close()'''
+def janelageometrica1(indicar,usar,nao):
+    indicar.geometry(str(usar)+'x'+str(nao))
+    with open('config/geom.txt', 'w') as geomdata:
+        geom = geomdata.write(str(usar)+'x'+str(nao))
+    geomdata.close()
 
 janela.bind('<space>s', lambda a:
     salvarA(janela, texto))
@@ -330,7 +268,6 @@ janela.bind('<space>n', lambda l:
 
 if __name__ == '__main__':
     barra_menu = Menu(janela)
-    barra_menu.configure(background=mBg, foreground=mFg)
     ficheiro_menu = Menu(barra_menu)
     ficheiro_menu.add_command(label=dd[11], command=lambda:
         abrir(janela))
@@ -345,28 +282,23 @@ if __name__ == '__main__':
     ficheiro_menu.add_separator()
     ficheiro_menu.add_command(label=dd[15], command=lambda:
         sair(janela))
-    
     editar_menu = Menu(barra_menu)
     editar_menu.add_command(label=dd[16], command=lambda:
         sobre('data/about.txt', 'r'))
     editar_menu.add_command(label=dd[17], command=lambda:
         ajuda('data/help.txt', 'r'))
     editar_menu.add_separator()
-    editar_menu.add_command(label=dd[40], command=website)
-    editar_menu.add_command(label=dd[41], command=repo)
-    
+    editar_menu.add_command(label=dd[37], command=website)
+    editar_menu.add_command(label=dd[38], command=repo)
     ver_menu = Menu(barra_menu)
     ver_1_m = Menu(ver_menu)
     ver_2_m = Menu(ver_menu)
     ver_3_m = Menu(ver_1_m)
     ver_5_m = Menu(ver_menu)
     ver_4_m = Menu(ver_5_m)
+    ver_6_m = Menu(ver_5_m)
     ver_7_m = Menu(ver_5_m)
     b_m = Menu(barra_menu)
-    
-    b_m.add_command(label=dd[38], command=new_window)
-    b_m.add_command(label=dd[39], command=relogio)
-    
     a_m = Menu(barra_menu)
     a_m.add_command(label='English (UK)', command=lambda:
         mudaIdioma('en', janela))
@@ -374,38 +306,40 @@ if __name__ == '__main__':
         mudaIdioma('br', janela))
     a_m.add_command(label='Português (Portugal)', command=lambda:
         mudaIdioma('pt', janela))
-    
+    a_m.add_separator()
+    a_m.add_command(label=dd[33], command=lambda:
+        formata(janela))
     ver_4_m.add_command(label=dd[18], command=lambda:
-        mudacor('white', 'black', 'black', 'black', 'white'))
+        mudacor('white', 'black', 'black'))
     ver_4_m.add_command(label=dd[19], command=lambda:
-        mudacor('black', 'white', 'white', 'dark grey', 'black'))
+        mudacor('black', 'white', 'white'))
     ver_4_m.add_separator()
+    ver_4_m.add_command(label=dd[20], command=lambda:
+        mudacor('light grey', 'black', 'black'))
     ver_4_m.add_command(label=dd[21], command=lambda:
-        mudacor('grey', 'black', 'black', 'black', 'white'))
-    
-    ver_7_m.add_command(label='Light Yellow', command=lambda:
-        mudacor('light yellow', 'black', 'black', '#f5b949', 'black'))
-    ver_7_m.add_command(label='Magic', command=lambda:
-        mudacor('purple', 'white', 'white', '#290340', 'white'))
-    ver_7_m.add_command(label='Through the Sky', command=lambda:
-        mudacor('light blue', 'black', 'black', '#031882', 'white'))
-    ver_7_m.add_command(label='Light Green', command=lambda:
-        mudacor('light green', 'black', 'black', '#0e2414', 'white'))
+        mudacor('dark grey', 'black', 'black'))
+    ver_4_m.add_command(label=dd[22], command=lambda:
+        mudacor('grey', 'black', 'black'))
+    ver_6_m.add_command(label='Light Yellow', command=lambda:
+        mudacor('light yellow', 'black', 'black'))
+    ver_6_m.add_command(label='Magic', command=lambda:
+        mudacor('purple', 'white', 'white'))
+    ver_6_m.add_command(label='Through the Sky', command=lambda:
+        mudacor('light blue', 'black', 'black'))
+    ver_6_m.add_command(label='Light Green', command=lambda:
+        mudacor('light green', 'black', 'black'))
     ver_7_m.add_command(label='Codetime', command=lambda:
-        mudacor('black', 'green', 'green', 'black', 'light green'))
-    ver_7_m.add_command(label='Darkest Night Ever', command=lambda:
-        mudacor('#040114', '#e8a78e', '#e8a78e', 'black', '#e8a78e'))
+        mudacor('black', 'green', 'green'))
     ver_7_m.add_command(label='Yellow Powershell', command=lambda:
-        mudacor('dark blue', 'yellow', 'yellow', '#020421', '#faf74d'))
+        mudacor('dark blue', 'yellow', 'yellow'))
     ver_7_m.add_command(label='Light Wizard', command=lambda:
-        mudacor('purple', 'light yellow', 'light yellow', '#290340', '#edecaf'))
+        mudacor('purple', 'light yellow', 'light yellow'))
     ver_7_m.add_command(label='Dark Forest', command=lambda:
-        mudacor('#0e2414', '#c0db7b', '#c0db7b', '#040d07', '#ccf0c5'))
+        mudacor('#0e2414', '#c0db7b', '#c0db7b'))
     ver_7_m.add_command(label='Christmas Night', command=lambda:
-       mudacor('#020421', '#a5a9e8', '#a5a9e8', '#020312', '#cbcef2'))
+       mudacor('#020421', '#a5a9e8', '#a5a9e8'))
     ver_7_m.add_command(label='Silent Night', command=lambda:
-        mudacor('#020421','pink', 'pink', '#020312', '#ebd1ed'))
-    
+        mudacor('#020421','pink', 'pink'))
     ver_2_m.add_command(label=dd[23],command=lambda:
         janelageometrica1(janela,600,400))
     ver_2_m.add_separator()
@@ -451,39 +385,19 @@ if __name__ == '__main__':
         janelageometrica1(janela,900,900))
     ver_2_m.add_command(label='1000x1000',command=lambda:
         janelageometrica1(janela,1000,1000))
-
-    ficheiro_menu.configure(background=corMenu, foreground=colorFg)
-    editar_menu.configure(background=corMenu, foreground=colorFg)
-    ver_menu.configure(background=corMenu, foreground=colorFg)
-    ver_1_m.configure(background=corMenu, foreground=colorFg)
-    ver_2_m.configure(background=corMenu, foreground=colorFg)
-    ver_3_m.configure(background=corMenu, foreground=colorFg)
-    ver_4_m.configure(background=corMenu, foreground=colorFg)
-    ver_5_m.configure(background=corMenu, foreground=colorFg)
-    ver_7_m.configure(background=corMenu, foreground=colorFg)
-    a_m.configure(background=corMenu, foreground=colorFg)
-    b_m.configure(background=corMenu, foreground=colorFg)
-
-'''    menu2 = Menu(novidade)
-    menu2.configure(background=mBg, foreground=mFg)
-
-menu2.add_command(label=dd[11], command=lambda:
-    abrir(novidade))
-menu2.add_command(label=dd[13], command=lambda:
-    salvarA(novidade))
-menu2.add_command(label=dd[12], command=lambda:
-    salvar(novidade))'''
+    b_m.add_command(label=dd[35], command=new_window)
+    b_m.add_command(label=dd[36], command=relogio)
 
 barra_menu.add_cascade(label=dd[24],menu=ficheiro_menu)
 barra_menu.add_cascade(label=dd[25],menu=ver_menu)
 ver_menu.add_cascade(label=dd[26], menu=ver_5_m)
 ver_5_m.add_cascade(label=dd[27], menu=ver_4_m)
+ver_5_m.add_cascade(label=dd[28], menu=ver_6_m)
 ver_5_m.add_cascade(label=dd[29], menu=ver_7_m)
 ver_menu.add_cascade(label=dd[30], menu=ver_2_m)
 barra_menu.add_cascade(label='Plugins', menu=b_m)
 barra_menu.add_cascade(label=dd[31], menu=a_m)
-barra_menu.add_cascade(label=dd[32], menu=editar_menu)
+barra_menu.add_cascade(label=dd[32],menu=editar_menu)
 
 janela.configure(menu=barra_menu)
-'''novidade.configure(menu=menu2)'''
 janela.mainloop()
