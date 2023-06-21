38de779fb6054098105e2d1722cf068d5bbbe0b7
MF366 <109810178+MF366-Coding@users.noreply.github.com>
2023-06-21 11:51:49 -0700

    UNSTABLE NOT WORKING. DO NOT DOWNLOAD!
    Instead download a release, the Latest one...

--- WriterClassic.py
+++ WriterClassic.py
@@ -186,12 +186,7 @@ implementation is available to the public in source code form.  A
 produce the work, or an object code interpreter used to run it.
 
   The "Corresponding Source" for a work in object code form means all
-the source code needed to generate, install, and (for an executable
-work) run the object code and to modify the work, including scripts to
-control those activities.  However, it does not include the work's
-System Libraries, or general-purpose tools or generally available free
-programs which are used unmodified in performing those activities but
-which are not part of the work.  For example, Corresponding Source
+the source code needed to generate, install, and (
 includes interface definition files associated with source files for
 the work, and the source code for shared libraries and dynamically
 linked subprograms that the work is specifically designed to require,
@@ -199,7 +194,7 @@ such as by intimate data communication or control flow between those
 subprograms and other parts of the work.
 
   The Corresponding Source need not include anything that users
-can regenerate automatically from other parts of the Corresponding
+can regenerate automatically from 
 Source.
 
   The Corresponding Source for a work in source code form is that
