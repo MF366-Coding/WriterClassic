ee3dee7f7bc832fe6c048534706aff9d9aef1dfa
MF366 <real_mf366@yahoo.com>
2023-06-21 19:27:15 +0100

    ?

--- WriterClassic.py
+++ WriterClassic.py
@@ -65,6 +65,7 @@ The LICENSE for simple_webbrowser is here:
 software and other kinds of works.
 
   The licenses for most software and other practical works are designed
+to take away your freedom to share and change the works.  By contrast,
 the GNU General Public License is intended to guarantee your freedom to
 share and change all versions of a program--to make sure it remains free
 software for all its users.  We, the Free Software Foundation, use the
