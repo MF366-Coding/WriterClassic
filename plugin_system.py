b9311b8b98fbffcf6a05adff3d14d5ef59a1ec36
norbcodes <thenorbert2008@gmail.com>
2023-08-29 09:31:03 +0200

    No no no I ain't importing nothing

--- plugin_system.py
+++ plugin_system.py
@@ -1,17 +1,19 @@
 # Hello world, Norb here.
 
-from WriterClassic import now, _LOG, plugin_dir
 from glob import glob
-
-from WriterClassic import desktop_win, TextWidget, config, data_dir, user_data, nix_assets, plugin_dir, locale
+# Look ma, no imports from WriterClassic!
+import datetime # Really, bro?
+now = datetime.datetime.now()
+# yes bro
 
 class Script:
     # The base script class.
     _GLOBALS = {}  # What ever variables the script can legally modify
     _BYTECODE = None
     _NAME = None
+    _LOGGER = None
 
-    def __init__(self, path: str, name: str, **_globals):
+    def __init__(self, path: str, name: str, logger, **_globals):
         """
         Load and compile a plugin.
 
@@ -24,7 +26,8 @@ class Script:
         self._BYTECODE = compile(path, name, 'exec')
         self._GLOBALS = _globals
         self._NAME = name
-        _LOG.write(f"{str(now)} - Lock and loaded plugin {self._NAME}!\n")
+        self._LOGGER = logger  # is this necessary? yeah, gotta log everything right? ;)
+        self._LOGGER.write(f"{str(now)} - Lock and loaded plugin {self._NAME}!\n")
 
     def SyncGlobalVars(self):
         """
@@ -38,7 +41,7 @@ class Script:
         """
         Run the actual thingy.
         """
-        _LOG.write(f"{str(now)} - Running script {self._NAME}!\n")
+        _LOGGER.write(f"{str(now)} - Running script {self._NAME}!\n")
         exec(self._BYTECODE, self._GLOBALS)
         # now obviously one should run verified plugins only, unless you potentially want your pc
         # to spontaneously combust.
@@ -50,18 +53,20 @@ class ScriptManager:
     def __init__(self):
         self._SCRIPTS = {}
 
-    def LoadPlugins(self):
+    def LoadPlugins(self, _globals: dict):
+        # call globals() in WriterClassic so we can access them here.
+        # I ain't importing shit!
         _files = glob(f"*.py", root_dir="./plugins/")  # sorry c++ plugins, though import DLLs might be a possibility...
         for file in _files:
             _name = file.replace(".py", "")
             self._SCRIPTS["file"] = Script (
-                file, _name,
-                desktop_win=desktop_win,
-                TextWidget=TextWidget,
-                config=config,
-                data_dir=data_dir,
-                user_data=user_data,
-                nix_assets=nix_assets,
-                plugin_dir=plugin_dir,
-                locale=locale
+                file, _name, _globals['_LOG'],
+                desktop_win     = _globals['desktop_win'],
+                TextWidget      = _globals['TextWidget'],
+                config          = _globals['config'],
+                data_dir        = _globals['data_dir'],
+                user_data       = _globals['user_data'],
+                nix_assets      = _globals['nix_assets'],
+                plugin_dir      = _globals['plugin_dir'],
+                locale          = _globals['locale']
             )
